4c11898ebdbf50c958d96a8736605131bef89081
j03yyyyy <joeykuay@gmail.com>
2025-09-28 23:26:40 +0800

    Program Setup
    The repository is initially created with a main.cpp file and basic project folder structure. This first commit set up the base for the project.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    srand(time(0)); // seed random generator
+    int moodChoice;
